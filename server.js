659a09b9f8fbd6d803f646c2b3611332b2daf480
atocchi <andrewtocchi@gmail.com>
2019-09-30 18:05:25 -0700

    --added variable for PORT for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require ('express');
 const app = express();
 const path = require("path");
 //declare port
-const PORT = 5000
+const PORT = process.env.PORT || 3030;
 
 
 //let express parse data
